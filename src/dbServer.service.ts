10e7f291bc51ce21dc5c7295ea16ff1aa28c4b8e
huangwenqiang <huangwenqiang@chain-c.com.com>
2020-03-30 02:38:42 +0800

    zhao bu dao module

---
+++ src/dbServer.service.ts
@@ -0,0 +1,29 @@
+import { Observable, throwError, from } from 'rxjs';
+// const { Client } = require('@elastic/elasticsearch')
+import { Client } from '@elastic/elasticsearch'
+
+
+export class DbService {
+    static readonly TAG = 'StoreUserService';
+    /**
+     * 添加一条store user 的 记录。==================OK
+     * @param newStoreUser  StoreUser
+     * @param oprator CommonRecordKey
+     */
+    static addStoreUser(): Observable<any> {
+        const client = new Client({
+            cloud: {
+                id: 'name:bG9jYWxob3N0JGFiY2QkZWZnaA==',
+            },
+            auth: {
+                username: 'elastic',
+                password: 'changeme'
+            }
+        })
+        return from(client.search({
+            index: 'my-index',
+            body: { foo: 'bar' }
+        }))
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,53 @@
-
-import { Server } from '@webserverless/fc-express';
+import { switchMap } from 'rxjs/operators';
 import express from 'express';
+import { throwError, of } from 'rxjs';
 const bodyParser = require('body-parser')
+import { Handler } from "express";
+import { DbService } from './dbServer.service';
+
 console.log('welcome')
-const app = express();
-app.all('*', (req, res) => {
-  console.log('qwe')
-  res.send('Hello serverless with TypeScript & Express5!');
-});
+// const app = express();
+// app.all('*', (req, res) => {
+//   console.log('qwe')
+//   res.send('Hello serverless with TypeScript & Express5!');
+// });
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-const server = new Server(app);
 
-// http trigger entry
-module.exports.handler = (req, res, context) => {
-  console.log('this');
-  // server.httpProxy(req, res, context);
-  res.send('Hello serverless with TypeScript & Express5!');
-  console.log('this2');
-  
-};
+export const handler: Handler = (
+  req: any,
+  res: any,
+  context: any,
+) => {
+
+  // switch (req..proxy) {
+  return DbService.addStoreUser().subscribe(
+    (data) => {
+      // makeCallBack(callback, 200, null, data);
+      console.log(data);
+      res.send('Hello serverless with TypeScript & Express5!');
+    },
+    (error) => {
+      console.log(error);
+      // makeCallBack(callback, 400, error);
+    }
+  )
+  // }
+}
+
+// app.use(bodyParser.json()) // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// const server = new Server(app);
+
+// // http trigger entry
+// module.exports.handler = (req, res, context) => {
+//   console.log(req);
+//   // server.httpProxy(req, res, context);
+//   res.send('Hello serverless with TypeScript & Express5!');
+//   console.log('this2');
+
+// };
 
 // api gateway entry
 // module.exports.handler = function(event, context, callback) {
