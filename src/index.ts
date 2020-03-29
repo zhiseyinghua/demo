5b5d2b660e9b4b07cb02a25844223b1d7d6ec18b
huangwenqiang <huangwenqiang@chain-c.com.com>
2020-03-30 01:11:22 +0800

    merge

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
+
 import { Server } from '@webserverless/fc-express';
 import express from 'express';
-
-const app = express()
+const bodyParser = require('body-parser')
+console.log('welcome')
+const app = express();
 app.all('*', (req, res) => {
+  console.log('qwe')
   res.send('Hello serverless with TypeScript & Express5!');
 });
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 const server = new Server(app);
 
 // http trigger entry
 module.exports.handler = (req, res, context) => {
-  server.httpProxy(req, res, context);
+  console.log('this');
+  // server.httpProxy(req, res, context);
+  res.send('Hello serverless with TypeScript & Express5!');
+  console.log('this2');
+  
 };
 
 // api gateway entry
