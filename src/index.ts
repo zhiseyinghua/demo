563231a51b9bceff5d7277bd78c5925dbabf6299
huangwenqiang <huangwenqiang@chain-c.com.com>
2020-10-08 20:19:15 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -8,59 +8,59 @@ const crypto = require('crypto');
 import edb64 from "en-decode-b64";
 
 
-
+console.log("aliyun start");
 
 module.exports.handler = function (req, res, context) {
+  console.log("aliyun start2");
   var url = req.url
   var path = req.path
+  console.log("url","path",url,path)
 
   getRawBody(req, function (err, data) {
-    console.log(data);
-    let jsstr = JSON.stringify(data);
-    let jsondata = JSON.parse(jsstr);
-    let buf = new Buffer(jsondata);
-    let buffdata = buf.toString();
-    var body = JSON.parse(buffdata);
+    var body = JSON.parse(data.toString());
+
     switch (path) {
       /**
        * 判断用户是否存在，如果是则发送验证码
        */
       case '/check_email':
-        let check = body as { email: string }
-        return DbService.addStoreUser(check.email).pipe(
-          map(
-            (data) => {
-              console.log(data && check.email);
-              if (data && data.email) {
-                console.log('User_already_exists');
-                throw ('User_already_exists')
-              } else {
-                console.log('You_can_register');
-                return 'You_can_register'
-              }
-            }),
-          // TODO:访问限制
-          switchMap((data) => {
-            return ToolService.SendEmail(check.email)
-          })
-        ).subscribe(
-          (data) => {
-            // makeCallBack(callback, 200, null, data);
-            res.send(JSON.stringify({ 'state': '200', 'message': 'success' }));
-          },
-          (error) => {
-            console.log('error', error['Error']);
-            console.log('error', error, typeof error);
-            res.send(JSON.stringify({ 'state': '400', 'message': error }));
-            // makeCallBack(callback, 400, error);
-          }
-        )
+        // let check = body as { email: string }
+        // return DbService.addStoreUser(check.email).pipe(
+        //   map(
+        //     (data) => {
+        //       console.log(data && check.email);
+        //       if (data && data.email) {
+        //         console.log('User_already_exists');
+        //         throw ('User_already_exists')
+        //       } else {
+        //         console.log('You_can_register');
+        //         return 'You_can_register'
+        //       }
+        //     }),
+        //   // TODO:访问限制
+        //   switchMap((data) => {
+        //     return ToolService.SendEmail(check.email)
+        //   })
+        // ).subscribe(
+        //   (data) => {
+        //     // makeCallBack(callback, 200, null, data);
+        //     res.send(JSON.stringify({ 'state': '200', 'message': 'success' }));
+        //   },
+        //   (error) => {
+        //     console.log('error', error['Error']);
+        //     console.log('error', error, typeof error);
+        //     res.send(JSON.stringify({ 'state': '400', 'message': error }));
+        //     // makeCallBack(callback, 400, error);
+        //   }
+        // )
         break;
       case '/greetings':
-        console.log('start');
-        signin(res)
+        console.log('greetings start');
+        // signin(res)
+        res.send(JSON.stringify({ 'state': '200', 'message': 'success' }));
         break;
       default:
+        return '123';
         break;
     }
 
